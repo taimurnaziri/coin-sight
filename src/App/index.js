59e26393a8272f405abe2130215034d6d2e50f09
tainazir <taimurknaziri@gmail.com>
2020-01-25 14:52:39 -0500

    Added components and fonts.

---
+++ src/App/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import styled, {css} from 'styled-components';
+
+const MyButton = styled.div`
+  color: green;
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WelcomeMessage/>
+        <MyButton>Hello</MyButton>
+        <MyButton primary>Hello</MyButton>
+        <TomatoButton primary>Hello</TomatoButton>
+      </div>
+    );
+  }
+}
+
+export default App;
