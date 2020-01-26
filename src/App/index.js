0da976fd9e4278d87b8c3462eb9a2d854c3c9a4b
tainazir <taimurknaziri@gmail.com>
2020-01-25 23:13:54 -0500

    Added app layout.

--- src/App/index.js
+++ src/App/index.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import WelcomeMessage from './WelcomeMessage';
-import styled, {css} from 'styled-components';
-
-const MyButton = styled.div`
-  color: green;
-  ${props => props.primary && css`
-    color: palevioletred;
-  `}
-`
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
-
+import AppLayout from './AppLayout';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
         <WelcomeMessage/>
-        <MyButton>Hello</MyButton>
-        <MyButton primary>Hello</MyButton>
-        <TomatoButton primary>Hello</TomatoButton>
-      </div>
+      </AppLayout>
     );
   }
 }
